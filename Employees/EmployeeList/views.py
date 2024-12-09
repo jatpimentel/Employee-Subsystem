4cbb504f3fc6488dba8e0346f253ef4d652a31fe
Edward Anthony R Quianzon <edwarquianzon@addu.edu.ph>
2024-12-09 21:13:27 +0800

    Added functionality to the radio button

--- Employees/EmployeeList/views.py
+++ Employees/EmployeeList/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
-from .models import person,role,employee,access_rights
-from .forms import personform,employeeform
+from .models import person, role, employee
+from .forms import personform, employeeform
 
 # Create your views here.
 def index(request):
-    return render (request,'index.html')
+    return render(request, 'index.html')
 
 def employee_list(request):
-    all_employees = employee.objects.all
-    return render (request,'employee-list.html', {'employees':all_employees})
+    # Default to 'all' if no status is selected
+    status_filter = request.GET.get('status', 'all')
+    
+    if status_filter == 'active':
+        employees = employee.objects.filter(employee_status='active')
+    elif status_filter == 'inactive':
+        employees = employee.objects.filter(employee_status='inactive')
+    else:  # Default to 'all'
+        employees = employee.objects.all()
+
+    context = {
+        'employees': employees,
+        'status_filter': status_filter,  # Pass status for debugging if needed
+    }
+    return render(request, 'employee-list.html', context)
+
 
 def employee_create(request):
     if request.method == "POST":
@@ -20,20 +33,17 @@ def employee_create(request):
             employee_instance = employee_form.save(commit=False)
             employee_instance.person = person_instance
             employee_instance.save()
-
             return redirect('employee') 
-        else:
-            print(person_form.errors)
-            print(employee_form.errors)
-
     else:
         person_form = personform()  
         employee_form = employeeform()  
 
-    return render(request, 'employee-create.html', {'person_form': person_form, 'employee_form': employee_form})
+    return render(request, 'employee-create.html', {
+        'person_form': person_form, 
+        'employee_form': employee_form
+    })
 
 def employee_edit(request, id):
-    # Fetch the existing employee instance
     employee_obj = get_object_or_404(employee, id=id)
     person_obj = employee_obj.person
 
@@ -47,38 +57,38 @@ def employee_edit(request, id):
             employee_form.instance.person = person_form.instance  
             employee_form.instance.save()  
             return redirect('employee') 
-        else:
-            print(person_form.errors)
-            print(employee_form.errors)
     else:
         person_form = personform(instance=person_obj)
         employee_form = employeeform(instance=employee_obj)
 
-    return render(request, 'employee-edit.html', {'employee': employee_obj, 'person_form': person_form, 'employee_form': employee_form})
+    return render(request, 'employee-edit.html', {
+        'employee': employee_obj, 
+        'person_form': person_form, 
+        'employee_form': employee_form
+    })
 
 def employee_view(request, id):
     employee_obj = get_object_or_404(employee, id=id)
     return render(request, 'employee-view.html', {'employee': employee_obj})
 
 def role_list(request):
-    all_roles = role.objects.all()  # Get all roles
-    role_counts = {}
-
-    for role_item in all_roles:  # Iterate over each role
-        role_counts[role_item] = employee.objects.filter(role=role_item).count()
-
-    # Pass the roles and counts to the template
-    return render(request, 'role-list.html', {'roles_with_counts': role_counts, 'roles': all_roles})
-
-
+    all_roles = role.objects.all()  
+    role_counts = {
+        role_item: employee.objects.filter(role=role_item).count()
+        for role_item in all_roles
+    }
+    return render(request, 'role-list.html', {
+        'roles_with_counts': role_counts, 
+        'roles': all_roles
+    })
 
 def role_create(request):
-    return render(request,'role-create.html')
+    return render(request, 'role-create.html')
 
 def role_view(request, id):
     role_obj = get_object_or_404(role, id=id)
-    return render(request,'role-view.html', {'role': role_obj})
+    return render(request, 'role-view.html', {'role': role_obj})
 
 def role_edit(request, id):
     role_obj = get_object_or_404(role, id=id)
-    return render(request,'role-edit.html' , {'role': role_obj})
+    return render(request, 'role-edit.html', {'role': role_obj})
